41fee78cd321a966ba4939cf2e28776b3a00b17f
Biggs Thorarensen <biggs@devillion.com>
2021-02-01 14:40:02 +0000

    html and css minifiers for build

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const htmlmin = require("html-minifier");
+ 
 module.exports = function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false);
  
@@ -7,5 +9,22 @@ module.exports = function (eleventyConfig) {
  
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
+  });
+ 
+  eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+    if (
+      process.env.ELEVENTY_PRODUCTION &&
+      outputPath &&
+      outputPath.endsWith(".html")
+    ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+ 
+    return content;
   });
 };
