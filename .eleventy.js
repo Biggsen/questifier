4f33df20cae7597e93eb1c5fe2b56500562738a8
Biggs Thorarensen <biggs@devillion.com>
2021-02-02 10:47:16 +0000

    style.css put in a css folder

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function (eleventyConfig) {
  
   eleventyConfig.addWatchTarget("./_tmp/style.css");
  
-  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./css/style.css" });
  
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
